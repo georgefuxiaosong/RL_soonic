04dbcbab5832b1fd6be054d1a3744fa775822243
georgefuxiaosong <32957961+georgefuxiaosong@users.noreply.github.com>
2018-06-30 16:33:06 +0800

    Update run_this.py

--- run_this.py
+++ run_this.py
@@ -71,79 +71,70 @@ def main():
     
     s_t=np.stack((x_t,x_t,x_t,x_t),axis=2) #开始的时候只有一帧图形，batch需要4帧作为一个state，我们把这一帧叠在一起,成为80*80*4的结构
     #x=cv2.cvtColor(cv2.resize(observation_, (80,80)), cv2.COLOR_BGR2YCR_CB)
-    
-    while True:
-        env.render() #渲染环境
+    for episode in range(3000):
+        while True:
+            env.render() #渲染环境
+            
+            #下面的动作选择，由于是每4帧图像重叠在一起作为一个state，那么我就把这4帧图像的动作设置成一样，意思就是每个动作重复执行4次
+            
+            if len(action_stack) > 0: #如果存放动作的栈不空，那么就从里面选择一个动作来执行
+                action_exute=action_stack.pop() #随意出栈4个动作的其中一个
+            else: #如果动作栈里面的 动作用完了，那么再次用state选出一个动作
+                action_button=env.choose_action(s_t) #根据这4帧图像选择动作,这个动作是自己设计的复合动作
+                action_system=env.action_button_2_onehot(action_button)#np.append(np.append(a0,a1),b1)# #转变为系统所能是被的动作
+                for i in range(3):
+                    action_stack.append(action_system) #把这个动作放到栈里面去，放3个，加上自己的一个，就是4个重复动作
+                action_exute=action_system
+            
+            observation_, reward, done, info=env.step(action_exute) #用当前动作获得下一帧的observation_， 当前的奖励等等信息
+            info_rings.append(info['rings']) #把执行当前动作获得的info里面的rings的个数存到info_rings的栈里面去
+            
+            if info_rings[1] -info_rings[0] < 0: #如果后面的一次动作使得rings的数量减少了，那么奖励值就要减少
+                reward-=50
+            elif info_rings[1] - info_rings[0] > 0: #如果后面的一次动作使得rings的数量增加了了，那么奖励值就要增加
+                reward+=50
+            else:
+                pass
+            
+            info_rings.popleft()
+            
+            #total_reward+=reward
+            #print(total_reward)
+            
+            #让他往右边走，所以在往右边走的这个动作上设计一个比较小的奖励
+            #if np.argmax(action_system)==7:
+            #   reward=0.1
+            
+            if done: #如果中途死掉了，就给一个负的奖励
+                reward=-100
+                env.reset()
+        
+            x_t1_pre=cv2.cvtColor(cv2.resize(observation_, (80,80)), cv2.COLOR_BGR2GRAY) #执行完一帧之后得到的图形数据，转换为80*80,并转换为灰度图
+            ret, x_t1=cv2.threshold(x_t1_pre, 1, 255, cv2.THRESH_BINARY) #对图像进行二值化转换，黑白两色
+            x_t1=np.reshape(x_t1,(80,80,1)) #把上面执行动作得到的一帧图像，转换变成80*80*1，
+            
+            #x_t1=RL.image_process(observation_[0]) #把上面执行完动作之后得到的一帧彩色图像observation_处理成黑白的80*80*1的大小
+            s_t1=np.append(s_t[:, :, :3], x_t1, axis=2) #把前面3帧和计算得到的1帧加起来作为下一个state
+            
+            
+            #把当前的状态s_t，当前采取的动作action,当前奖励reward，下一个状态s_t1，是否结束的标志存起来
+            #需要注意的是当前的状态s_t和下一个状态s_t1是4帧图像叠起来的，所以需要在前面进行处理
+            
+            env.store_memory(s_t, action_button, reward, s_t1, done) #存的时候还是存自己设计的复合动作按键
+            
+            if (total_steps > 3000) and (total_steps%100==0): #在步数超过200补之后开始学习，并且每50步学习一次
+                with tf.device("/gpu:0"):
+                    env.trainNet() #开始训练网络，学习
            
-        #下面的动作选择，由于是每4帧图像重叠在一起作为一个state，那么我就把这4帧图像的动作设置成一样，意思就是每个动作重复执行4次
-        
-        if len(action_stack) > 0: #如果存放动作的栈不空，那么就从里面选择一个动作来执行
-            action_exute=action_stack.pop() #随意出栈4个动作的其中一个
-        else: #如果动作栈里面的 动作用完了，那么再次用state选出一个动作
-            action_button=env.choose_action(s_t) #根据这4帧图像选择动作,这个动作是自己设计的复合动作
-            action_system=env.action_button_2_onehot(action_button)#np.append(np.append(a0,a1),b1)# #转变为系统所能是被的动作
-            for i in range(3):
-                action_stack.append(action_system) #把这个动作放到栈里面去，放3个，加上自己的一个，就是4个重复动作
-            action_exute=action_system
+            if done:
+                break
             
-        observation_, reward, done, info=env.step(action_exute) #用当前动作获得下一帧的observation_， 当前的奖励等等信息
-        info_rings.append(info['rings']) #把执行当前动作获得的info里面的rings的个数存到info_rings的栈里面去
-        
-        if info_rings[1] -info_rings[0] < 0: #如果后面的一次动作使得rings的数量减少了，那么奖励值就要减少
-            reward-=50
-        elif info_rings[1] - info_rings[0] > 0: #如果后面的一次动作使得rings的数量增加了了，那么奖励值就要增加
-            reward+=50
-        else:
-            pass
+            s_t=s_t1
             
-        info_rings.popleft()
-
-        #total_reward+=reward
-        #print(total_reward)
-        
-        #让他往右边走，所以在往右边走的这个动作上设计一个比较小的奖励
-        #if np.argmax(action_system)==7:
-        #   reward=0.1
-        
-        if done: #如果中途死掉了，就给一个负的奖励
-            reward=-100
-            env.reset()
-        
-        x_t1_pre=cv2.cvtColor(cv2.resize(observation_, (80,80)), cv2.COLOR_BGR2GRAY) #执行完一帧之后得到的图形数据，转换为80*80,并转换为灰度图
-        ret, x_t1=cv2.threshold(x_t1_pre, 1, 255, cv2.THRESH_BINARY) #对图像进行二值化转换，黑白两色
-        x_t1=np.reshape(x_t1,(80,80,1)) #把上面执行动作得到的一帧图像，转换变成80*80*1，
-        
-        #x_t1=RL.image_process(observation_[0]) #把上面执行完动作之后得到的一帧彩色图像observation_处理成黑白的80*80*1的大小
-        s_t1=np.append(s_t[:, :, :3], x_t1, axis=2) #把前面3帧和计算得到的1帧加起来作为下一个state
-        
-        
-        #把当前的状态s_t，当前采取的动作action,当前奖励reward，下一个状态s_t1，是否结束的标志存起来
-        #需要注意的是当前的状态s_t和下一个状态s_t1是4帧图像叠起来的，所以需要在前面进行处理
-        
-        env.store_memory(s_t, action_button, reward, s_t1, done) #存的时候还是存自己设计的复合动作按键
-        
-        if (total_steps > 3000) and (total_steps%100==0): #在步数超过200补之后开始学习，并且每50步学习一次
-            with tf.device("/gpu:0"):
-                env.trainNet() #开始训练网络，学习
-           
-        if done:
-            break
-        
-        s_t=s_t1
-        
-        total_steps+=1
-            
-        if total_steps % 50000==0:
-            saver.save(sess, './', global_step=total_steps)
-        
-        '''state=''
-        if total_steps < RL.observe_pic:
-            state='Observe'
-        else:
-            state='Train'
-            print("TIMESTEP", RL.learn_step_counter, "/ STATE", state, \
-                  "/ EPSILON", RL.epsilon,  "/ REWARD", observation, \
-                  "/ Q_MAX %e" % np.max(action))'''
+            total_steps+=1
+                
+            if total_steps % 50000==0:
+                saver.save(sess, './', global_step=total_steps)
             
     print('Game over')
     env.close()
